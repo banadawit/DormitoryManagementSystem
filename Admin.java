187051aef1401cd4d3138e8b7530a80ec2fffe83
banadawit <129886937+banadawit@users.noreply.github.com>
2023-12-22 23:41:06 -0800

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,1 +1,21 @@
+package Dormitory;
 
+import java.io.IOException;
+import java.sql.*;
+import java.util.Scanner;
+
+interface AdminPage {
+    void registerNewStudent();
+
+    void addProcter();
+
+    public void removeStudent(String name);
+
+    void DisplayStudentList();
+
+    void displayBuildingDetail(String name);
+}
+// using interface
+interface IdGenerator {
+    int generateId();
+}
