1aab4f240c05d98180d161e5b40802b7c727753a
ayu10up <filedugasa@gmail.com>
2023-12-24 02:16:20 -0800

    asking user to insert his/her username and password

--- Dorm_Access.java
+++ Dorm_Access.java
@@ -9,8 +9,8 @@ class Dorm_Room extends Buildings {
     static int bedcounter = 0;
     String roomName;
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/dormitory";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "banned1234";
+    private static final String USERNAME = "user_name";
+    private static final String PASSWORD = "password";
 
     public static void addRoom(String rm) {
         try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)) {
