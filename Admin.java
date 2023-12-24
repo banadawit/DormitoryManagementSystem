3c598f986240cd4c2a1792d318603814f3cb81b0
banadawit <banadawithunde@gmail.com>
2023-12-23 23:33:36 -0800

    about password,user name and database name

--- Admin.java
+++ Admin.java
@@ -20,9 +20,13 @@ interface IdGenerator {
     int generateId();
 }
 public class Admin extends PersonInfo implements AdminPage, Constants {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/dormitory";
-    private static final String USERNAME = "ayana";
-    private static final String PASSWORD = "ayu10upme";
+
+    // you have to insert your real database name
+    // and also your user name in terms of user_name
+    // and also your password in terms of user_password
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/database_name";
+    private static final String USERNAME = "user_name";
+    private static final String PASSWORD = "user_password";
 
     private Connection connection;
 
